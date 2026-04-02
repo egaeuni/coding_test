c4c670d320ab585e386dce54449c06594d9bc2b1
egaeuni <163407321+egaeuni@users.noreply.github.com>
2026-04-02 13:21:03 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,24 @@
+N = int(input())
+M = int(input())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+visited = [False for _ in range(N+1)]
+count = 0
+
+def dfs(v):
+  global count
+  visited[v] = True
+
+  for i in graph[v]:
+    if not visited[i]:
+      count += 1
+      dfs(i)
+
+dfs(1)
+print(count)
